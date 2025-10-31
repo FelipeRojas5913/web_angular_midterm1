fc3196d24fc5b7b523e9308e4bd6dbf742d23fbc
Felipe Rojas <jrojasce@unal.edu.co>
2025-10-30 21:32:58 -0500

    feat: enhance vehicle list page to display vehicle count by brand

--- src/app/vehicle/pages/vehicle-list-page/vehicle-list-page.ts
+++ src/app/vehicle/pages/vehicle-list-page/vehicle-list-page.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    computed,
+    inject,
+    OnInit,
+    signal,
+} from '@angular/core';
 import { Vehicle } from '../../interfaces/vehicle.interface';
 import { VehicleService } from '../../services/vehicle.service';
 
@@ -12,6 +19,15 @@ import { VehicleService } from '../../services/vehicle.service';
 export class VehicleListPage implements OnInit {
     public readonly vehicles = signal<Vehicle[]>([]);
     private readonly vehicleService = inject(VehicleService);
+    private readonly vehiclesCountMap = signal<Record<string, number>>({});
+
+    public readonly vehiclesCount = computed(() => {
+        const countMap = this.vehiclesCountMap();
+        return Object.entries(countMap).map(([marca, count]) => ({
+            marca,
+            count,
+        }));
+    });
 
     ngOnInit(): void {
         this.getVehicles();
@@ -21,10 +37,28 @@ export class VehicleListPage implements OnInit {
         this.vehicleService.getVehicles().subscribe({
             next: (vehiclesResponse) => {
                 this.vehicles.set(vehiclesResponse);
+                this.processCount(vehiclesResponse);
             },
             error: (error) => {
                 console.error('Error fetching vehicles:', error);
             },
         });
+    }
+
+    private processCount(vehicles: Vehicle[]) {
+        for (const vehicle of vehicles) {
+            if (this.vehiclesCountMap().hasOwnProperty(vehicle.marca)) {
+                const count = this.vehiclesCountMap()[vehicle.marca] || 0;
+                this.vehiclesCountMap.set({
+                    ...this.vehiclesCountMap(),
+                    [vehicle.marca]: count + 1,
+                });
+            } else {
+                this.vehiclesCountMap.set({
+                    ...this.vehiclesCountMap(),
+                    [vehicle.marca]: 1,
+                });
+            }
+        }
     }
 }
